f89596fcb7bd5e4e550eadeb542722ddd630f38f
mbenhaddou <mbenhaddou@mentis-consulting.be>
2021-04-06 19:29:52 +0200

    bug fix

--- kdmt/ml/metrics/plot/__init__.py
+++ kdmt/ml/metrics/plot/__init__.py
@@ -1,12 +1,12 @@
 """
 Plotting functions
 """
-from .classification import (confusion_matrix,
+from kdmt.ml.metrics.classification import (confusion_matrix,
                              feature_importances,
                              precision_at_proportions)
 from .grid_search import grid_search
 from .learning_curve import learning_curve
-from .metrics import metrics_at_thresholds
+from kdmt.ml.metrics.metrics import metrics_at_thresholds
 from .precision_recall import precision_recall
 from .roc import roc
 from .validation_curve import validation_curve
